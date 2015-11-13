10239223e12397bb44263334c32229b8b6af1bbf
Rachel Lim <email": null>
2015-11-13 17:14:49 -0500

    users controller tests created and passing

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe UsersController do
 
   context '#create' do
 
-
     it 'creates a new user with valid params' do
       expect{
         post :create, user: FactoryGirl.attributes_for(:user)
@@ -21,6 +20,18 @@ RSpec.describe UsersController do
       expect{
         post :create, user: FactoryGirl.attributes_for(:question)
       }.to_not change(User, :count)
+    end
+
+    it 'redirects to the root_path when the user is successfully created' do
+      expect(
+          post :create, user: FactoryGirl.attributes_for(:user)
+      ).to redirect_to root_path
+    end
+
+    it 'redirects to the root_path when the user is successfully created' do
+      expect(
+          post :create, user: FactoryGirl.attributes_for(:question)
+      ).to redirect_to register_path
     end
   end
 end
