5e65600a034ed172df1697eec1d889452bedf13c
Rachel Lim <email": null>
2015-11-13 16:48:24 -0500

    Add all model tests. All tests pass.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.create(username: "ders", password: "password")
-user.questions.create(title: "Test?", body: "this is a test-body")
-Question.first.answers.create(content: "this is your answer")
-user2 = User.create(username: "Dom", password: "password")
-Answer.first.update_attributes(user_id: 2)
-Question.first.comments.create(note: "this is a question comment", user_id: 2)
-Answer.first.comments.create(note: "this is an answer comment", user_id: 1)
-Question.first.tags.create(description: "awesomesauce")
-Comment.first.votes.create(value: 1, user_id: 1)
-Question.first.votes.create(value: -1, user_id: 2)
+# #
+# # Examples:
+# #
+# #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# #   Mayor.create(name: 'Emanuel', city: cities.first)
+# user = User.create(username: "ders", password: "password")
+# user.questions.create(title: "Test?", body: "this is a test-body")
+# Question.first.answers.create(content: "this is your answer")
+# user2 = User.create(username: "Dom", password: "password")
+# Question.first.comments.create(note: "this is a question comment", user_id: 2)
+# Answer.first.comments.create(note: "this is an answer comment", user_id: 1)
+# Question.first.tags.create(description: "awesomesauce")
+# Comment.first.votes.create(value: 1, user_id: 1)
+# Question.first.votes.create(value: -1, user_id: 2)
