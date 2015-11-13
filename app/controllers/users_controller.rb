8aae38c7dc81fad9232a00ae28bf6386f06bdb3f
Angelita Graves <angelita.l.graves@vanderbilt.edu>
2015-11-13 12:06:53 -0500

    include user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      redirect root_path
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect register_path
+      redirect_to register_path
     end
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
