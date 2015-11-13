3d4d2ae96978d44110860fab1f5005b717296118
Rebecca Kleinberg <rebeccakleinberg@gmail.com>
2015-11-13 14:45:26 -0500

    can add answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,17 @@
-def create
-  redirect #to qq page
+class AnswersController < ApplicationController
+
+  def create
+    answer = current_user.answers.build(answer_params)
+    if answer.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content,:question_id)
+  end
 end
