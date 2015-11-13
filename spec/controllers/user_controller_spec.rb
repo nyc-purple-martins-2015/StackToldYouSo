971383eaa6a049b80b6c7e2857cb17a368773be5
Rachel Lim <email": null>
2015-11-13 17:08:01 -0500

    adds user controller spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe UsersController do
+  let(:sample_user) { FactoryGirl.build(:user) }
 
-  
-  
+  it '#new' do
+    get :new
+    expect(assigns(:user)).to be_a_kind_of(User)
+  end
+
+  context '#create' do
+
+
+    it 'creates a new user with valid params' do
+      expect{
+        post :create, user: FactoryGirl.attributes_for(:user)
+      }.to change(User, :count).by(1)
+    end
+
+    it 'does not create create a user when params are invalid' do
+      expect{
+        post :create, user: FactoryGirl.attributes_for(:question)
+      }.to_not change(User, :count)
+    end
+  end
 end
