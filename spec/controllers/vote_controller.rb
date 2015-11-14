8c35d8e634212727e42559895a91ddd184224e75
rachelylim <rachel.y.lim@gmail.com>
2015-11-14 16:04:46 -0500

    Commit to rebase master.

--- spec/controllers/vote_controller.rb
+++ spec/controllers/vote_controller.rb
@@ -1,15 +1,27 @@
 equire 'rails_helper'
 
 RSpec.describe VotesController do
-  let(:sample_user) { FactoryGirl.build(:user) }
+
+  before(:each) do
+    user = stub_authorize_user!
+  let(:sample_question) { FactoryGirl.create(:question) } 
+  end
 
   context '#create' do
 
-    it 'user can vote on a question' do
+    it 'user can upvote on a question' do
       expect{
-        post :create
-        vote: FactoryGirl.attributes_for(:vote)
-      }.to change(User, :count).by(1)
+        post :create,
+        vote: {:value => 1, :user_id => user.id, :votable_type => "Question", :votable_id => sample_question.id}
+      }.to change(sample_question.vote_count, :count).by(1)
     end
+
+    it 'user can downvote on a question' do
+      expect{
+        post :create,
+        vote: {:value => -1, :user_id => user.id, :votable_type => "Question", :votable_id => sample_question.id}
+      }.to change(sample_question.vote_count, :count).by(-1)
+    end
+
   end
 end
