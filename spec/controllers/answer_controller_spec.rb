0878fa932abddbad9984e604857d2d330b5a33a6
Anders Drew <anders.j.drew@gmail.com>
2015-11-15 11:11:13 -0500

    add some tests for answers controller

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController do
+  before(:each) do
+    stub_authorize_user!
+    user = FactoryGirl.create(:user)
+    stub_current_user(user)
+    @question = FactoryGirl.create(:question)
+  end
+  context '#create' do
+    it 'creates a new answer with valid params' do
+      expect{
+        post :create, answer: FactoryGirl.attributes_for(:answer)
+      }.to change(Answer, :count).by(1)
+    end
+    it 'does not create a new question with invalid params' do
+    end
+  end
 end
