48930b29c02625af395b465bf4bace9d34b35165
Anders Drew <anders.j.drew@gmail.com>
2015-11-15 10:40:51 -0500

    rspecs for invalid answer comment

--- spec/controllers/comment_controller_spec.rb
+++ spec/controllers/comment_controller_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe CommentsController do
         post :create, comment: {:note => "testnote", :user_id => @sample_user.id, :commentable_type => "Answer", :commentable_id => sample_answer.id}
       }.to change(Comment, :count).by(1)
     end
+    it 'does not create a new comment for an answer with invalid params' do
+      expect{
+        post :create, comment: {:note => "testnote"}
+      }.to_not change(Comment, :count)
+    end
   end
 end
 
