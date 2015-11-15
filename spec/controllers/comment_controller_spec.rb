2bda799f3b707a886724c702bfee637e61381ca8
Anders Drew <anders.j.drew@gmail.com>
2015-11-15 10:34:03 -0500

    rspec for creating an answer comment with valid params

--- spec/controllers/comment_controller_spec.rb
+++ spec/controllers/comment_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController do
+  before(:each) do
+    stub_authorize_user!
+    @sample_user = FactoryGirl.create(:user)
+  end
+  context '#creates' do
+    sample_answer = FactoryGirl.create(:answer)
+    it 'creates a new comment for an answer with valid params' do
+      expect{
+        post :create, comment: {:note => "testnote", :user_id => @sample_user.id, :commentable_type => "Answer", :commentable_id => sample_answer.id}
+      }.to change(Comment, :count).by(1)
+    end
+  end
 end
+
